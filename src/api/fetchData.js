9d3f0ac2af7e5983de5d6848446c3dd81ca45cba
RoyBotech <roy@botech.studio>
2024-07-07 18:04:35 +0300

    add the fetch data from mongoDB, show the data in the UI and create HTML file for any diamond

---
+++ src/api/fetchData.js
@@ -0,0 +1,22 @@
+const fetchData = async () => {
+  try {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ0ZXN0dXNlciIsImlhdCI6MTcyMDM2MTg1OSwiZXhwIjoxNzIwMzY1NDU5fQ.dkCO6mmBg7Z26I9ZVHpGi6X7ONtdnUaAnJUxVi9xwMw";
+
+    const response = await fetch("http://localhost:5000/yerushalmi/diamonds", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("Fetched data:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+export default fetchData;
