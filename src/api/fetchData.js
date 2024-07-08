4ca3e8d7eb71f78b14976160082997a0bb27a037
RoyBotech <roy@botech.studio>
2024-07-08 17:54:47 +0300

    send API request to the server to create HTML files for the selected Diamonds

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,8 @@
 const fetchData = async () => {
   try {
+    //need to generate from the SERVER
     const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ0ZXN0dXNlciIsImlhdCI6MTcyMDM2NjIzNywiZXhwIjoxNzIwMzY5ODM3fQ.hYP3WIQsvz7odvHF27NyaiZXqYbnZY_LFuTkJnVnhic";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ0ZXN0dXNlciIsImlhdCI6MTcyMDQ0NjgwMSwiZXhwIjoxNzIwNDUwNDAxfQ.eGr_ChZv9MjWp0kMBA6zUXlirGxJsnTlk-GXsp559dk";
 
     const response = await fetch("http://localhost:5000/yerushalmi/diamonds", {
       headers: {
